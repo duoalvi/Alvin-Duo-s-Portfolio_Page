e1d7cd83fbb8141016f6495078b2dffbe657b386
duoalvi <138685503+duoalvi@users.noreply.github.com>
2024-04-13 13:20:35 +0000

    Add files via upload

---
+++ port_web.js
@@ -0,0 +1,21 @@
+// window.addEventListener('load', function() {
+    // const loaderOverlay = document.querySelector('.loader-overlay');
+    // loaderOverlay.classList.add('loaded');
+// });
+
+var type = new Typed('#js_web', {
+    strings:["Front-end" , "YouTuber" , "Mobile Developer" , "UI/UX"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+var type = new Typed('#wecl_js', {
+    strings:["TO" , "My" , "Portfolio_Page"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+  
